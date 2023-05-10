a81c4977be00435e49751c425ffdccfbb107a3b2
Jakob Roithinger <jake.roithinger@gmail.com>
2023-05-10 17:02:40 +0200

    change outputs to svg in pygraphviz_test.py

--- graph_visualization/pygraphviz_test.py
+++ graph_visualization/pygraphviz_test.py
@@ -1,4 +1,5 @@
 from multilayered_graph.multilayer_graph_generator import generate_multilayer_graph
+from node_sorting.barycenter_heuristic import few_gaps_barycenter_sort
 
 ml_graph = generate_multilayer_graph(4, 12, 0.2, 0.5, randomness_seed=1)
 
@@ -9,10 +10,14 @@ pgv_graph = ml_graph.to_pygraphviz_graph()
 
 # print(pgv_graph.string())  # print to screen
 # pgv_graph.write("simple.dot")  # write to simple.dot
-pgv_graph.draw("mlg.png")
+pgv_graph.draw("plain.svg")
+
+few_gaps_barycenter_sort(ml_graph)
+pgv_graph_barycenter = ml_graph.to_pygraphviz_graph()
+pgv_graph_barycenter.draw("barycenter.svg")
 
 pgv_graph.layout(prog="dot")
-pgv_graph.draw("dot.png")
+pgv_graph.draw("dot.svg")
 
 x = pgv_graph.nodes()
 print(x)
