b866accbf422e305214daca87d48ebbacd868561
Jakob Roithinger <jake.roithinger@gmail.com>
2023-05-10 18:45:20 +0200

    tested few_gaps_barycenter_smart_sort with pygraphviz

--- graph_visualization/pygraphviz_test.py
+++ graph_visualization/pygraphviz_test.py
@@ -1,5 +1,8 @@
 from multilayered_graph.multilayer_graph_generator import generate_multilayer_graph
-from node_sorting.barycenter_heuristic import few_gaps_barycenter_sort
+from node_sorting.barycenter_heuristic import (
+    few_gaps_barycenter_sort,
+    few_gaps_barycenter_smart_sort,
+)
 
 ml_graph = generate_multilayer_graph(4, 12, 0.2, 0.5, randomness_seed=1)
 
@@ -10,16 +13,11 @@ pgv_graph = ml_graph.to_pygraphviz_graph()
 
 # print(pgv_graph.string())  # print to screen
 # pgv_graph.write("simple.dot")  # write to simple.dot
-pgv_graph.draw("plain.svg")
-
-few_gaps_barycenter_sort(ml_graph)
+# pgv_graph.draw("plain.svg")
+few_gaps_barycenter_smart_sort(ml_graph)
+# few_gaps_barycenter_sort(ml_graph)
 pgv_graph_barycenter = ml_graph.to_pygraphviz_graph()
 pgv_graph_barycenter.draw("barycenter.svg")
 
-pgv_graph.layout(prog="dot")
-pgv_graph.draw("dot.svg")
-
-x = pgv_graph.nodes()
-print(x)
-print(x[0])
-print(type(x[0]))
+# pgv_graph.layout(prog="dot")
+# pgv_graph.draw("dot.svg")
