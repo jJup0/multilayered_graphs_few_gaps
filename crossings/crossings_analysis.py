d2f5700f9299c3a51bb144abc076dd4166b7fe3d
Jakob Roithinger <jake.roithinger@gmail.com>
2023-06-01 11:34:43 +0200

    crossings_analysis.py refactor

--- crossings/crossings_analysis.py
+++ crossings/crossings_analysis.py
@@ -48,16 +48,18 @@ class CrossingsAnalyser:
         self.algorithms: list[NamedAlgorithm] = [
             NamedAlgorithm("Barycenter naive", few_gaps_barycenter_sort_naive),
             NamedAlgorithm("Barycenter improved", few_gaps_barycenter_smart_sort),
-            NamedAlgorithm("Barycenter split", few_gaps_barycenter_split),
+            # NamedAlgorithm("Barycenter split", few_gaps_barycenter_split), # identical to improved
             NamedAlgorithm("Median naive", few_gaps_median_sort_naive),
             NamedAlgorithm("Median improved", few_gaps_median_sort_improved),
-            NamedAlgorithm("Constrained nodes (paper impl)", few_gaps_constrained_paper),
+            NamedAlgorithm(
+                "Constrained nodes (paper impl)", few_gaps_constrained_paper
+            ),
             NamedAlgorithm("Gurobi", few_gaps_gurobi),
         ]
         self.algs_graphtype_crossings: dict[NamedAlgorithm, dict[str, list[int]]] = {}
         self.graph_type_names: dict[str, str] = {}
         self.timings: dict[str, list[float]] = {
-            "name_gen": [],
+            # "name_gen": [],
             "graph_gen": [],
             # "reorg": [],
             "deep_copies": [],
@@ -71,27 +73,46 @@ class CrossingsAnalyser:
         }
 
         try:
-            for i in range(100):
+            for i in range(4):
                 random_params = {
-                    "node_count": 20,
+                    "node_count": 30,
                     "layers_count": 4,
-                    "edge_density": 0.05,
+                    "edge_density": 0.02,
                     "long_edge_probability": 0.3,
                 }
+                two_layer_params = {
+                    "layer1_count": 20,
+                    "layer2_count": 10,
+                    "virtual_nodes_count": 20,
+                    "regular_edges_count": 400,
+                }
+                # two_layer_params = {
+                #     "layer1_count": 3,
+                #     "layer2_count": 3,
+                #     "virtual_nodes_count": 3,
+                #     "regular_edges_count": 8,
+                # }
+                # self._test_random_graph(**random_params, randomness_seed=i)
+                #
+                # random_params["long_edge_probability"] = 0.7
+                # self._test_random_graph(**random_params, randomness_seed=i)
+                #
+                # random_params["layers_count"] = 2
 
-                self._test_random_graph(**random_params, randomness_seed=i)
-
-                random_params["long_edge_probability"] = 0.7
-                self._test_random_graph(**random_params, randomness_seed=i)
-
-                random_params["layers_count"] = 2
-                self._test_random_graph(**random_params, randomness_seed=i)
-
-                random_params["layers_count"] = 8
-                self._test_random_graph(**random_params, randomness_seed=i)
+                # self._reorganize_graph_and_count_crossings(
+                #     self._gen_random_graph(**random_params, randomness_seed=i),
+                #     self.algs_graphtype_crossings,
+                # )
+                self._reorganize_graph_and_count_crossings(
+                    self._gen_two_layer_graph(**two_layer_params),
+                    self.algs_graphtype_crossings,
+                )
+                #
+                # random_params["layers_count"] = 8
+                # self._test_random_graph(**random_params, randomness_seed=i)
                 print(f"Round {i}")
-        except:
-            print(f"Exception, stopping")
+        except KeyboardInterrupt:
+            print(f"Keyboard interrupt, stopping")
             # stop and show results so far
 
         for graph_type in self.graph_type_names.values():
@@ -115,16 +136,37 @@ class CrossingsAnalyser:
             avg_time_str = f"{total_time_ms / len(times):.3f}"
             print(f"{timing_name:>30} took avg: {avg_time_str:>7}ms")
 
-    def _test_random_graph(
-            self,
-            layers_count: int,
-            node_count: int,
-            edge_density: float,
-            long_edge_probability: float,
-            *,
-            randomness_seed: int | None = None,
+    def _gen_two_layer_graph(
+        self,
+        layer1_count: int,
+        layer2_count: int,
+        virtual_nodes_count: int,
+        regular_edges_count: int,
+    ) -> GraphAndType:
+        t1 = time.perf_counter_ns()
+
+        ml_graph = multilayer_graph_generator.generate_two_layer_graph(
+            layer1_count=layer1_count,
+            layer2_count=layer2_count,
+            virtual_nodes_count=virtual_nodes_count,
+            regular_edges_count=regular_edges_count,
+        )
+        name = f"2-layer random {layer1_count} x {layer2_count}; v={virtual_nodes_count}; e={regular_edges_count}"
+
+        self.graph_type_names[name] = name
+
+        self.timings["graph_gen"].append(time.perf_counter_ns() - t1)
+        return GraphAndType(graph=ml_graph, type_name=name)
+
+    def _gen_random_graph(
+        self,
+        layers_count: int,
+        node_count: int,
+        edge_density: float,
+        long_edge_probability: float,
+        *,
+        randomness_seed: int | None = None,
     ):
-        perf_timer_start__name_gen = time.perf_counter_ns()
         (
             graph_short_str,
             graph_long_str,
@@ -146,33 +188,20 @@ class CrossingsAnalyser:
             randomness_seed=randomness_seed,
         )
         perf_timer_graph_gen__reorg = time.perf_counter_ns()
-
-        graph_and_type = GraphAndType(
-            graph=graph, type_name=self.graph_type_names[graph_short_str]
-        )
-        # graphs_to_test_on.append(graph)
-        self._reorganize_graph_and_count_crossings(
-            graph_and_type, self.algs_graphtype_crossings
-        )
-        # perf_timer_reorg__end = time.perf_counter_ns()
-
-        # note timings
-        self.timings["name_gen"].append(
-            perf_timer_name_gen__graph_gen - perf_timer_start__name_gen
-        )
         self.timings["graph_gen"].append(
             perf_timer_graph_gen__reorg - perf_timer_name_gen__graph_gen
         )
-        # self.timings["reorg"].append(
-        #     perf_timer_reorg__end - perf_timer_graph_gen__reorg
-        # )
+        graph_and_type = GraphAndType(
+            graph=graph, type_name=self.graph_type_names[graph_short_str]
+        )
+        return graph_and_type
 
     def _reorganize_graph_and_count_crossings(
-            self,
-            graph_and_type: GraphAndType,
-            algorithms_to_graph_structure_to_crossings: dict[
-                NamedAlgorithm, dict[str, list[int]]
-            ],
+        self,
+        graph_and_type: GraphAndType,
+        algorithms_to_graph_structure_to_crossings: dict[
+            NamedAlgorithm, dict[str, list[int]]
+        ],
     ):
         perf_timer_start__deep_copy = time.perf_counter_ns()
 
@@ -194,7 +223,7 @@ class CrossingsAnalyser:
         named_alg: NamedAlgorithm
         graph_copy: MultiLayeredGraph
         for named_alg, graph_copy in zip(
-                algorithms_to_graph_structure_to_crossings.keys(), graph_copies, strict=True
+            algorithms_to_graph_structure_to_crossings.keys(), graph_copies, strict=True
         ):
             named_alg.algorithm(graph_copy)
             crossing_count = graph_copy.get_total_crossings()
