cf4d4dfe592df5c7e2d854ec589c69e245b219f4
Jakob Roithinger <jake.roithinger@gmail.com>
2023-07-02 18:12:58 +0200

    added sample graph to draw with pygraphviz

--- graph_visualization/pygraphviz_test.py
+++ graph_visualization/pygraphviz_test.py
@@ -3,6 +3,7 @@ from crossing_minimization.barycenter_heuristic import (
     few_gaps_barycenter_smart_sort,
 )
 from crossing_minimization.median_heuristic import few_gaps_median_sort_improved
+from multilayered_graph.multilayered_graph import MultiLayeredGraph
 
 
 def draw_graph():
@@ -25,5 +26,43 @@ def draw_graph():
     # pgv_graph.draw("dot.svg")
 
 
+def draw_presentation_graph():
+    # print(pgv_graph.string())  # print to screen
+    # pgv_graph.write("simple.dot")  # write to simple.dot
+    # pgv_graph.draw("plain.svg")
+    ml_graph = MultiLayeredGraph(layer_count=3)
+    node_f = ml_graph.add_real_node(0, "f")
+    node_d = ml_graph.add_real_node(0, "d")
+    node_h = ml_graph.add_real_node(0, "h")
+    node_i = ml_graph.add_real_node(0, "i")
+
+    node_j = ml_graph.add_real_node(1, "j")
+    node_e = ml_graph.add_real_node(1, "e")
+    node_g = ml_graph.add_real_node(1, "g")
+
+    node_a = ml_graph.add_real_node(2, "a")
+    node_b = ml_graph.add_real_node(2, "b")
+    node_c = ml_graph.add_real_node(2, "c")
+
+    ml_graph.add_edge(node_f, node_j)
+    ml_graph.add_edge(node_h, node_j)
+    ml_graph.add_edge(node_i, node_j)
+    ml_graph.add_edge(node_h, node_g)
+    ml_graph.add_edge(node_d, node_e)
+
+    ml_graph.add_edge(node_d, node_a)
+    ml_graph.add_edge(node_i, node_b)
+    ml_graph.add_edge(node_i, node_c)
+
+    ml_graph.add_edge(node_e, node_a)
+    ml_graph.add_edge(node_e, node_b)
+    ml_graph.add_edge(node_e, node_c)
+
+    pgv_graph = ml_graph.to_pygraphviz_graph()
+
+    pgv_graph.layout(prog="dot")
+    pgv_graph.draw("presentation_dot.svg")
+
+
 if __name__ == "__main__":
-    draw_graph()
+    draw_presentation_graph()
