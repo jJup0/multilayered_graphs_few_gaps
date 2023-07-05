e9634299a7a70b05dc0c4c90615fcb7efd6173dc
Jakob Roithinger <jake.roithinger@gmail.com>
2023-07-05 07:51:04 +0200

    major refactor to crossings analysis + functionality to prepare data to be plotted

--- crossings/crossings_analysis.py
+++ crossings/crossings_analysis.py
@@ -8,11 +8,15 @@ from crossing_minimization.barycenter_heuristic import (
     few_gaps_barycenter_smart_sort,
     few_gaps_barycenter_sort_naive,
 )
-from crossing_minimization.constrained_implementation import few_gaps_constrained_paper
-from crossing_minimization.gurobi_int_lin import few_gaps_gurobi
+from crossing_minimization.gurobi_int_lin import few_gaps_gurobi_wrapper
 from crossing_minimization.median_heuristic import (
     few_gaps_median_sort_improved,
     few_gaps_median_sort_naive,
+)
+from crossings.crossing_analysis_visualization import (
+    DataSet,
+    GraphLabels,
+    draw_crossing_analysis_graph,
 )
 from multilayered_graph import multilayer_graph_generator
 from multilayered_graph.multilayered_graph import MultiLayeredGraph
@@ -44,82 +48,138 @@ class GraphAndType:
 
 class CrossingsAnalyser:
     def __init__(self):
+        # todo test two layer cross minimization
         self.algorithms: list[NamedAlgorithm] = [
             NamedAlgorithm("Barycenter naive", few_gaps_barycenter_sort_naive),
             NamedAlgorithm("Barycenter improved", few_gaps_barycenter_smart_sort),
-            # NamedAlgorithm("Barycenter split", few_gaps_barycenter_split), # identical to improved
             NamedAlgorithm("Median naive", few_gaps_median_sort_naive),
             NamedAlgorithm("Median improved", few_gaps_median_sort_improved),
-            NamedAlgorithm(
-                "Constrained nodes (paper impl)", few_gaps_constrained_paper
-            ),
-            NamedAlgorithm("Gurobi", few_gaps_gurobi),
+            NamedAlgorithm("Gurobi", few_gaps_gurobi_wrapper),
         ]
-        self.algs_graphtype_crossings: dict[NamedAlgorithm, dict[str, list[int]]] = {}
-        self.graph_type_names: dict[str, str] = {}
+        self.algs_graphtype_time_ns: dict[
+            NamedAlgorithm, dict[str, list[int]]
+        ] = defaultdict(lambda: defaultdict(list))
+        self.algs_graphtype_crossings: dict[
+            NamedAlgorithm, dict[str, list[int]]
+        ] = defaultdict(lambda: defaultdict(list))
+        self.graph_type_names: list[str] = []
+
+        # time taken to perform actions
         self.timings: dict[str, list[float]] = {
-            # "name_gen": [],
             "graph_gen": [],
-            # "reorg": [],
             "deep_copies": [],
         }
-        for named_alg in self.algorithms:
-            self.timings[named_alg.name] = []
+        # for named_alg in self.algorithms:
+        #     self.timings[named_alg.name] = []
 
     def analyse_crossings(self):
-        self.algs_graphtype_crossings = {
-            alg: defaultdict(list) for alg in self.algorithms
-        }
+        # clear previous data
+        self.algs_graphtype_time_ns.clear()
+        self.algs_graphtype_crossings.clear()
 
-        runs = 5
-        try:
-            for i in range(runs):
-                # random_params = {
-                #     "node_count": 30,
-                #     "layers_count": 4,
-                #     "edge_density": 0.02,
-                #     "long_edge_probability": 0.3,
-                # }
-                two_layer_params = {
-                    "layer1_count": 7,
-                    "layer2_count": 7,
-                    "virtual_nodes_count": 7,
-                    "regular_edges_count": 15,
-                }
-                self._reorganize_graph_and_count_crossings(
-                    self._gen_two_layer_graph(**two_layer_params),
-                    self.algs_graphtype_crossings,
+        # run_parameters[i] = (layer2_count, layer1_count, virtual_nodes_count, regular_edges_count)
+        two_layer_graph_parameters: list[tuple[int, int, int, int]] = [
+            (7, 7, 7, 15),
+            (10, 10, 10, 20),
+            (10, 3, 10, 15),
+            (10, 40, 10, 10),
+            (40, 10, 10, 10),
+        ]
+
+        one_sided_algorithm_kwargs = {"one_sided": True}
+        two_sided_algorithm_kwargs = {"max_iterations": 3, "one_sided": False}
+        for alg_kwargs in (one_sided_algorithm_kwargs, two_sided_algorithm_kwargs):
+            try:
+                for i, (
+                    l1_count,
+                    l2_count,
+                    vnode_count,
+                    reg_edges_count,
+                ) in enumerate(two_layer_graph_parameters):
+                    random_2_layer_graph = self._generate_random_two_layer_graph(
+                        layer1_count=l1_count,
+                        layer2_count=l2_count,
+                        virtual_nodes_count=vnode_count,
+                        regular_edges_count=reg_edges_count,
+                    )
+                    for algorithm in self.algorithms:
+                        self._minimize_and_count_crossings(
+                            random_2_layer_graph,
+                            algorithm,
+                            alg_kwargs,
+                        )
+
+                    print(f"Round {i}")
+            except KeyboardInterrupt:
+                # stop and show results so far
+                print(f"Keyboard interrupt, stopping")
+
+            self._print_crossing_results()
+
+    def analyze_crossings_for_graph_two_layer(self):
+        # clear previous data
+        self.algs_graphtype_time_ns.clear()
+        self.algs_graphtype_crossings.clear()
+
+        # algorithms_to_test = [alg for alg in self.algorithms if alg.name != "Gurobi"]
+        algorithms_to_test = [alg for alg in self.algorithms]
+
+        two_sided_algorithm_kwargs = {"max_iterations": 3, "one_sided": False}
+
+        iterations = 10
+        rounds = 5
+        scaling = [1 + 0.3 * round_nr for round_nr in range(rounds)]
+        base_real_nodes = 8
+        base_virtual_nodes = 4
+        regular_edges_density = 0.1
+
+        graph_x_values: list[float] = []
+        for round_nr in range(rounds):
+            real_nodes = int(base_real_nodes * scaling[round_nr])
+            graph_x_values.append(real_nodes)
+            virtual_nodes = int(base_virtual_nodes * scaling[round_nr])
+            regular_edge_count = int(real_nodes * real_nodes * regular_edges_density)
+            for iteration in range(iterations):
+                print(
+                    f"Round {round_nr}, iteration {iteration}. {real_nodes=}, {regular_edge_count=}"
                 )
-                # two_layer_params = {
-                #     "layer1_count": 5,
-                #     "layer2_count": 3,
-                #     "virtual_nodes_count": 3,
-                #     "regular_edges_count": 3,
-                # }
-                # self._test_random_graph(**random_params, randomness_seed=i)
-                #
-                # random_params["long_edge_probability"] = 0.7
-                # self._test_random_graph(**random_params, randomness_seed=i)
-                #
-                # random_params["layers_count"] = 2
 
-                # self._reorganize_graph_and_count_crossings(
-                #     self._gen_random_graph(**random_params, randomness_seed=i),
-                #     self.algs_graphtype_crossings,
-                # )
-                # self._reorganize_graph_and_count_crossings(
-                #     self._gen_two_layer_graph(**two_layer_params),
-                #     self.algs_graphtype_crossings,
-                # )
-                #
-                # random_params["layers_count"] = 8
-                # self._test_random_graph(**random_params, randomness_seed=i)
-                print(f"Round {i}")
-        except KeyboardInterrupt:
-            print(f"Keyboard interrupt, stopping")
-            # stop and show results so far
+                random_2_layer_graph = self._generate_random_two_layer_graph(
+                    layer1_count=real_nodes,
+                    layer2_count=real_nodes,
+                    virtual_nodes_count=virtual_nodes,
+                    regular_edges_count=regular_edge_count,
+                    override_name=f"Round {round_nr}",
+                )
 
-        for graph_type in self.graph_type_names.values():
+                for algorithm in algorithms_to_test:
+                    self._minimize_and_count_crossings(
+                        random_2_layer_graph,
+                        algorithm,
+                        two_sided_algorithm_kwargs,
+                    )
+
+        # print(dict(self.algs_graphtype_crossings))
+        # print(dict(self.algs_graphtype_time_ns))
+        data_sets: list[DataSet] = []
+        for algorithm in algorithms_to_test:
+            y_values: list[float] = []
+            for round_nr in range(rounds):
+                graph_name = f"Round {round_nr}"
+                crossings = self.algs_graphtype_crossings[algorithm][graph_name]
+                y_values.append(sum(crossings) / len(crossings))
+
+            data_sets.append(DataSet(algorithm.name, y_values))
+
+        graph_labels = GraphLabels(
+            "# real nodes (half the amount of virtual nodes)",
+            "crossings",
+            "Crossings after minimization",
+        )
+        draw_crossing_analysis_graph(graph_x_values, data_sets, graph_labels)
+
+    def _print_crossing_results(self):
+        for graph_type in self.graph_type_names:
             print(f'For graph type "{graph_type}":')
             for named_alg in self.algorithms:
                 total_crossings = sum(
@@ -140,12 +200,14 @@ class CrossingsAnalyser:
             avg_time_str = f"{total_time_ms / len(times):.3f}"
             print(f"{timing_name:>30} took avg: {avg_time_str:>7}ms")
 
-    def _gen_two_layer_graph(
+    def _generate_random_two_layer_graph(
         self,
         layer1_count: int,
         layer2_count: int,
         virtual_nodes_count: int,
         regular_edges_count: int,
+        *,
+        override_name: str | None = None,
     ) -> GraphAndType:
         t1 = time.perf_counter_ns()
 
@@ -155,14 +217,17 @@ class CrossingsAnalyser:
             virtual_nodes_count=virtual_nodes_count,
             regular_edges_count=regular_edges_count,
         )
-        name = f"2-layer random {layer1_count} x {layer2_count}; v={virtual_nodes_count}; e={regular_edges_count}"
+        if override_name is None:
+            name = f"2-layer random {layer1_count} x {layer2_count}; v={virtual_nodes_count}; e={regular_edges_count}"
+        else:
+            name = override_name
 
-        self.graph_type_names[name] = name
+        self.graph_type_names.append(name)
 
         self.timings["graph_gen"].append(time.perf_counter_ns() - t1)
         return GraphAndType(graph=ml_graph, type_name=name)
 
-    def _gen_random_graph(
+    def _generate_random_graph(
         self,
         layers_count: int,
         node_count: int,
@@ -171,17 +236,14 @@ class CrossingsAnalyser:
         *,
         randomness_seed: int | None = None,
     ):
-        (
-            graph_short_str,
-            graph_long_str,
-        ) = multilayer_graph_generator.random_graph_to_short_long_str(
+        graph_long_str = multilayer_graph_generator.random_graph_to_long_str(
             layers_count,
             node_count,
             edge_density,
             long_edge_probability,
             None,
         )
-        self.graph_type_names[graph_short_str] = graph_long_str
+        self.graph_type_names.append(graph_long_str)
         perf_timer_name_gen__graph_gen = time.perf_counter_ns()
 
         graph: MultiLayeredGraph = multilayer_graph_generator.generate_multilayer_graph(
@@ -195,61 +257,27 @@ class CrossingsAnalyser:
         self.timings["graph_gen"].append(
             perf_timer_graph_gen__reorg - perf_timer_name_gen__graph_gen
         )
-        graph_and_type = GraphAndType(
-            graph=graph, type_name=self.graph_type_names[graph_short_str]
-        )
+        graph_and_type = GraphAndType(graph=graph, type_name=graph_long_str)
         return graph_and_type
 
-    def _reorganize_graph_and_count_crossings(
+    def _minimize_and_count_crossings(
         self,
         graph_and_type: GraphAndType,
-        algorithms_to_graph_structure_to_crossings: dict[
-            NamedAlgorithm, dict[str, list[int]]
-        ],
+        named_alg: NamedAlgorithm,
+        algorithm_kwargs: dict[str, Any],
     ):
-        DRAW_GRAPHS = False
-        perf_timer_start__deep_copy = time.perf_counter_ns()
-
         original_graph = graph_and_type.graph
-        graph_type = graph_and_type.type_name
-        if DRAW_GRAPHS and False:
-            original_graph.to_pygraphviz_graph().draw(f"original.svg")
 
-        graph_copies: list[MultiLayeredGraph] = [
-            copy.deepcopy(original_graph)
-            for _ in range(len(algorithms_to_graph_structure_to_crossings))
-        ]
-
-        self.timings["deep_copies"].append(
-            time.perf_counter_ns() - perf_timer_start__deep_copy
+        graph_copy: MultiLayeredGraph = copy.deepcopy(original_graph)
+        self.time_algorithm_and_count_crossings(
+            named_alg, graph_and_type, algorithm_kwargs
         )
 
-        perf_timer_end_prev_alg = time.perf_counter_ns()
-
-        # for some reason zip has faulty type hinting, so type hint here:
-        named_alg: NamedAlgorithm
-        graph_copy: MultiLayeredGraph
-        for named_alg, graph_copy in zip(
-            algorithms_to_graph_structure_to_crossings.keys(), graph_copies, strict=True
-        ):
-            named_alg.algorithm(graph_copy)
-            # optional validation step
-            try:
-                self.assert_sorted_graph_valid(original_graph, graph_copy, 2)
-            except AssertionError as err:
-                print(f"WARNING!!! {named_alg.name} sorted the graph invalidly: {err}")
-
-            crossing_count = graph_copy.get_total_crossings()
-            algorithms_to_graph_structure_to_crossings[named_alg][graph_type].append(
-                crossing_count
-            )
-            perf_timer_end_curr_alg = time.perf_counter_ns()
-            self.timings[named_alg.name].append(
-                perf_timer_end_curr_alg - perf_timer_end_prev_alg
-            )
-            perf_timer_end_prev_alg = perf_timer_end_curr_alg
-
-            # print(f"{named_alg.name} produced {crossing_count} crossings")
+        # optional validation step, can be left out if correctness is guaranteed
+        try:
+            self.assert_sorted_graph_valid(original_graph, graph_copy, 2)
+        except AssertionError as err:
+            print(f"WARNING!!! {named_alg.name} sorted the graph invalidly: {err}")
 
     def assert_sorted_graph_valid(
         self,
@@ -257,6 +285,14 @@ class CrossingsAnalyser:
         sorted_graph: MultiLayeredGraph,
         gaps_allowed: int,
     ):
+        """Given an original graph, check if it's crossing-minimized graph is a valid permutation.
+
+        Args:
+            orignal_graph (MultiLayeredGraph): The original graph.
+            sorted_graph (MultiLayeredGraph): The crossing-minimized graph.
+            gaps_allowed (int): The amount of gaps allowed in the crossing-minimized graph.
+        """
+
         def assert_with_message(evaluation: bool, msg: str):
             if not evaluation:
                 raise AssertionError(msg)
@@ -290,6 +326,25 @@ class CrossingsAnalyser:
                 f"Produced {gaps} gaps, but only {gaps_allowed} gaps allowed",
             )
 
+    def time_algorithm_and_count_crossings(
+        self,
+        named_alg: NamedAlgorithm,
+        graph_and_type: GraphAndType,
+        algorithm_kwargs: dict[str, Any],
+    ):
+        graph = graph_and_type.graph
+        graph_name = graph_and_type.type_name
+
+        start_ns = time.perf_counter_ns()
+        named_alg.algorithm(graph, **algorithm_kwargs)
+        total_time_ns = time.perf_counter_ns() - start_ns
+
+        self.algs_graphtype_time_ns[named_alg][graph_name].append(total_time_ns)
+
+        crossing_count = graph.get_total_crossings()
+        self.algs_graphtype_crossings[named_alg][graph_name].append(crossing_count)
+
 
 if __name__ == "__main__":
-    CrossingsAnalyser().analyse_crossings()
+    # CrossingsAnalyser().analyse_crossings()
+    CrossingsAnalyser().analyze_crossings_for_graph_two_layer()
