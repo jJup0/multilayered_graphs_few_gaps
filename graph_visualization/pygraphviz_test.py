98e83a02e01f08ca3808d7ac801b6df73c4b3fa3
Jakob Roithinger <jake.roithinger@gmail.com>
2023-07-17 16:49:43 +0200

    pgv test unimportant changes

--- graph_visualization/pygraphviz_test.py
+++ graph_visualization/pygraphviz_test.py
@@ -1,21 +1,19 @@
-from crossing_minimization.barycenter_heuristic import few_gaps_barycenter_smart_sort
-from crossing_minimization.median_heuristic import few_gaps_median_sort_improved
 from multilayered_graph.multilayer_graph_generator import generate_multilayer_graph
 from multilayered_graph.multilayered_graph import MultiLayeredGraph
 
 
 def draw_graph():
     ml_graph = generate_multilayer_graph(5, 16, 0.2, 0.5, randomness_seed=1)
-
+    print(ml_graph.get_total_crossings())
     # first_node = ml_graph.all_nodes_as_list()[0]
     #
     # pgv_graph = ml_graph.to_pygraphviz_graph()
     # pos = ml_graph.nodes_positions()
 
-    few_gaps_barycenter_smart_sort(ml_graph)
-    few_gaps_median_sort_improved(ml_graph)
-    pgv_graph_median = ml_graph.to_pygraphviz_graph()
-    pgv_graph_median.draw("median.svg")  # type: ignore # type of draw partially unknown
+    # few_gaps_barycenter_smart_sort(ml_graph)
+    # few_gaps_median_sort_improved(ml_graph)
+    # pgv_graph_median = ml_graph.to_pygraphviz_graph()
+    # pgv_graph_median.draw("median.svg")  # type: ignore # type of draw partially unknown
 
     # print(pgv_graph.string())  # print to screen
     # pgv_graph.write("simple.dot")  # write to simple.dot
