26920387f58390267135d24c27ca60407565861a
Jakob Roithinger <jake.roithinger@gmail.com>
2023-07-17 06:03:20 +0200

    graph sorting utils: added get_graph_neighbors_from_above_or_below() generate_layers_to_above_or_below() GraphSorter(ABC)

--- crossing_minimization/utils.py
+++ crossing_minimization/utils.py
@@ -1,8 +1,10 @@
-from typing import Any, Callable, Literal, NoReturn, TypeVar
+from abc import ABC, abstractmethod
+from typing import Any, Callable, Literal, NoReturn, TypeAlias, TypeVar
 
 from multilayered_graph.multilayered_graph import MultiLayeredGraph
 
 T = TypeVar("T")
+Above_or_below_T: TypeAlias = Literal["above"] | Literal["below"]
 
 DEFAULT_MAX_ITERATIONS_MULTILAYERED_CROSSING_MINIMIZATION = 3
 
@@ -11,7 +13,7 @@ def lgraph_sorting_algorithm(func: T) -> T:
     """Decorator for function which sorts a layered graph.
 
     Function head should look like this:
-    (ml_graph: MultiLayeredGraph, *, max_iterations: int = 3, one_sided_if_two_layers: bool = False) -> None
+    (ml_graph: MultiLayeredGraph, *, max_iterations: int = 3, only_one_up_iteration: bool = False) -> None
 
     Does not modify the function in any way.
     """
@@ -29,41 +31,31 @@ def deprecated_lgraph_sorting(func: Callable[..., Any]) -> Callable[..., NoRetur
     return wrapper
 
 
-def sorting_parameter_check(
-    ml_graph: MultiLayeredGraph, *, max_iterations: int, one_sided_if_two_layers: bool
-):
-    # input parameter validation
-    if not (one_sided_if_two_layers is True or one_sided_if_two_layers is False):
-        raise ValueError(
-            f'one_side must be true or false, received "{one_sided_if_two_layers}"'
-        )
-    if one_sided_if_two_layers:
-        if ml_graph.layer_count != 2:
-            raise ValueError(
-                f"One-sided crossing minimization can only be performed on graphs with exactly 2 layers."
-                f"Input graph has {ml_graph.layer_count} layers."
-            )
-    elif max_iterations <= 0:
-        raise ValueError(f"iterations must be > 0, received {max_iterations}")
-
-
-def get_layer_idx_above_or_below(
-    layer_idx: int, above_or_below: Literal["above"] | Literal["below"]
-):
+def get_layer_idx_above_or_below(layer_idx: int, above_or_below: Above_or_below_T):
     if above_or_below == "above":
         return layer_idx + 1
     if above_or_below == "below":
         return layer_idx - 1
-    raise ValueError(f'{above_or_below} needs to be either "above" or "below".')
+    raise ValueError(f'Parameter must be "above" or "below", got: {above_or_below}')
+
+
+def get_graph_neighbors_from_above_or_below(
+    ml_graph: MultiLayeredGraph, above_or_below: Above_or_below_T
+):
+    if above_or_below == "below":
+        return ml_graph.nodes_to_in_edges
+    if above_or_below == "above":
+        return ml_graph.nodes_to_out_edges
+    raise ValueError(f'Parameter must be "above" or "below", got: {above_or_below}')
 
 
 def generate_layers_to_above_or_below(
-    ml_graph: MultiLayeredGraph, max_iterations: int, one_sided_if_two_layers: bool
-) -> list[tuple[int, Literal["above"] | Literal["below"]]]:
-    layers_to_above_below: list[tuple[int, Literal["above"] | Literal["below"]]] = [
+    ml_graph: MultiLayeredGraph, max_iterations: int, only_one_up_iteration: bool
+) -> list[tuple[int, Above_or_below_T]]:
+    layers_to_above_below: list[tuple[int, Above_or_below_T]] = [
         (layer_idx, "below") for layer_idx in range(1, ml_graph.layer_count)
     ]
-    if not one_sided_if_two_layers:
+    if not only_one_up_iteration:
         layers_to_above_below.extend(
             (layer_idx, "above")
             for layer_idx in range(ml_graph.layer_count - 2, -1, -1)
@@ -72,3 +64,182 @@ def generate_layers_to_above_or_below(
         layers_to_above_below *= max_iterations
 
     return layers_to_above_below
+
+
+class GraphSorter(ABC):
+    algorithm_name = "<set algorithm name>"
+
+    @classmethod
+    def sort_graph(
+        cls,
+        ml_graph: MultiLayeredGraph,
+        *,
+        max_iterations: int,
+        only_one_up_iteration: bool,
+        side_gaps_only: bool,
+        max_gaps: int,
+    ) -> None:
+        """Sorts the nodes of a multilayered graph.
+
+        Args:
+            ml_graph (MultiLayeredGraph): The graph to sort.
+            max_iterations (int): Maximum up & down iterations for sorting layers.
+            only_one_up_iteration (bool): Whether to only do one up iteration or not.
+            side_gaps_only (bool): Whether gaps are only allowed to be on the side or not.
+            max_gaps (int): Maximum amount of gaps allowed.
+        """
+        cls._sorting_parameter_check(
+            ml_graph,
+            max_iterations=max_iterations,
+            only_one_up_iteration=only_one_up_iteration,
+            side_gaps_only=side_gaps_only,
+            max_gaps=max_gaps,
+        )
+        return cls._sort_graph(
+            ml_graph,
+            max_iterations=max_iterations,
+            only_one_up_iteration=only_one_up_iteration,
+            side_gaps_only=side_gaps_only,
+            max_gaps=max_gaps,
+        )
+
+    @classmethod
+    @abstractmethod
+    def _sort_graph(
+        cls,
+        ml_graph: MultiLayeredGraph,
+        *,
+        max_iterations: int,
+        only_one_up_iteration: bool,
+        side_gaps_only: bool,
+        max_gaps: int,
+    ) -> None:
+        """Implementation detail of sort graph. Should be overriden"""
+        raise NotImplementedError("not implemented")
+
+    # minor todo make into wrapper
+    @classmethod
+    def _sorting_parameter_check(
+        cls,
+        ml_graph: MultiLayeredGraph,
+        *,
+        max_iterations: int,
+        only_one_up_iteration: bool,
+        side_gaps_only: bool,
+        max_gaps: int,
+    ):
+        # input parameter validation
+        if not (only_one_up_iteration is True or only_one_up_iteration is False):
+            raise ValueError(
+                f'one_side must be true or false, received "{only_one_up_iteration}"'
+            )
+        if only_one_up_iteration:
+            if ml_graph.layer_count != 2:
+                raise ValueError(
+                    f"One-sided crossing minimization can only be performed on graphs with exactly 2 layers."
+                    f"Input graph has {ml_graph.layer_count} layers."
+                )
+        elif max_iterations <= 0:
+            raise ValueError(f"iterations must be > 0, received {max_iterations}")
+
+        if side_gaps_only:
+            if max_gaps != 2:
+                raise ValueError(
+                    f"If side gaps only, max_gaps must be equal to 2. Got {side_gaps_only}"
+                )
+        else:
+            if max_gaps <= 0:
+                raise ValueError(
+                    f"Maximum allowed gaps must be greater than 0. Got {max_gaps}"
+                )
+
+    @classmethod
+    def sort_2layered_sidegaps_onesided(
+        cls,
+        ml_graph: MultiLayeredGraph,
+    ) -> None:
+        assert ml_graph.layer_count == 2
+        return cls._sort_graph(
+            ml_graph,
+            side_gaps_only=True,
+            only_one_up_iteration=True,
+            max_gaps=2,
+            max_iterations=1,
+        )
+
+    @classmethod
+    def sort_2layered_sidegaps_twosided(
+        cls,
+        ml_graph: MultiLayeredGraph,
+        *,
+        max_iterations: int = DEFAULT_MAX_ITERATIONS_MULTILAYERED_CROSSING_MINIMIZATION,
+    ) -> None:
+        assert ml_graph.layer_count == 2
+        return cls._sort_graph(
+            ml_graph,
+            side_gaps_only=True,
+            only_one_up_iteration=False,
+            max_gaps=2,
+            max_iterations=max_iterations,
+        )
+
+    @classmethod
+    def sort_2layered_kgaps_onesided(
+        cls, ml_graph: MultiLayeredGraph, *, max_gaps: int
+    ) -> None:
+        assert ml_graph.layer_count == 2
+        return cls._sort_graph(
+            ml_graph,
+            side_gaps_only=True,
+            only_one_up_iteration=False,
+            max_gaps=max_gaps,
+            max_iterations=1,
+        )
+
+    @classmethod
+    def sort_2layered_kgaps_twosided(
+        cls,
+        ml_graph: MultiLayeredGraph,
+        *,
+        max_gaps: int,
+        max_iterations: int = DEFAULT_MAX_ITERATIONS_MULTILAYERED_CROSSING_MINIMIZATION,
+    ):
+        assert ml_graph.layer_count == 2
+        return cls._sort_graph(
+            ml_graph,
+            side_gaps_only=False,
+            max_gaps=max_gaps,
+            only_one_up_iteration=False,
+            max_iterations=max_iterations,
+        )
+
+    @classmethod
+    def sort_multilayered_sidegaps(
+        cls,
+        ml_graph: MultiLayeredGraph,
+        *,
+        max_iterations: int = DEFAULT_MAX_ITERATIONS_MULTILAYERED_CROSSING_MINIMIZATION,
+    ):
+        return cls._sort_graph(
+            ml_graph,
+            side_gaps_only=True,
+            max_gaps=2,
+            only_one_up_iteration=False,
+            max_iterations=max_iterations,
+        )
+
+    @classmethod
+    def sort_multilayered_kgaps(
+        cls,
+        ml_graph: MultiLayeredGraph,
+        *,
+        max_gaps: int,
+        max_iterations: int = DEFAULT_MAX_ITERATIONS_MULTILAYERED_CROSSING_MINIMIZATION,
+    ):
+        return cls._sort_graph(
+            ml_graph,
+            side_gaps_only=False,
+            max_gaps=max_gaps,
+            only_one_up_iteration=False,
+            max_iterations=max_iterations,
+        )
