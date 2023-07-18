9aab7f34fc01dd73b9d0fc1a4e21bda16e402f70
Jakob Roithinger <jake.roithinger@gmail.com>
2023-07-18 17:47:11 +0200

    gurobi formatted solution output printing

--- crossing_minimization/gurobi_int_lin.py
+++ crossing_minimization/gurobi_int_lin.py
@@ -177,42 +177,54 @@ def gurobi_one_sided(
         if m.Status != GRB.OPTIMAL:
             raise Exception(f"Model is not optimal: {m.Status}")
 
-        if gap_constraint_vars is not None and real_node_neighbor_vars is not None:
-            total_gaps = 0
-            for var in gap_constraint_vars:
-                total_gaps += var.X
-            print(f"{len(gap_constraint_vars)} gap vars. Sum/{total_gaps=}")
-
-            total_neighbors = 0
-            for var in real_node_neighbor_vars:
-                total_neighbors += var.X
-            print(
-                f"{len(real_node_neighbor_vars)} neighbor vars. Sum/{total_neighbors=}"
+        print_model_info = True
+        if print_model_info:
+            _gurobi_result_info(
+                ml_graph,
+                ordering_gp_vars=ordering_gp_vars,
+                gap_constraint_vars=gap_constraint_vars,
+                real_node_neighbor_vars=real_node_neighbor_vars,
             )
-
-        # TEMP DEBUG
-        virtual_nodes = [n for n in ml_graph.layers_to_nodes[1] if n.is_virtual]
-        real_nodes = [n for n in ml_graph.layers_to_nodes[1] if not n.is_virtual]
-
-        rnodes_left_of_rnode_vars: dict[MLGNode, float] = {
-            rnode_right: sum(
-                ordering_gp_vars[rnode_left, rnode_right].X
-                for rnode_left in real_nodes
-                if rnode_left is not rnode_right
-            )
-            for rnode_right in real_nodes
-        }
-        print(f"{sorted(rnodes_left_of_rnode_vars.items(), key=lambda x: x[1])}")
-
-        print(f"total real nodes: {len(real_nodes)}")
-        for vnode in virtual_nodes:
-            left_of_rnode_constraints = sum(
-                ordering_gp_vars[vnode, rnode].X for rnode in real_nodes
-            )
-            print(f"{vnode}: {left_of_rnode_constraints=}")
 
         # order graph using gurobi variables
         gurobi_merge_sort(nodes, ordering_gp_vars)
+
+
+def _gurobi_result_info(
+    ml_graph: MultiLayeredGraph,
+    *,
+    ordering_gp_vars: dict[MLGNodeEdge_T, gp.Var],
+    gap_constraint_vars: list[gp.Var] | None,
+    real_node_neighbor_vars: list[gp.Var] | None,
+):
+    if gap_constraint_vars is not None and real_node_neighbor_vars is not None:
+        total_gaps = 0
+        for var in gap_constraint_vars:
+            total_gaps += var.X
+        print(f"{len(gap_constraint_vars)} gap vars. Sum/{total_gaps=}")
+        right_gap_var = gap_constraint_vars[-1]
+        left_gap_var = gap_constraint_vars[-2]
+        print(
+            f"{right_gap_var.VarName} = {right_gap_var.X} || {left_gap_var.VarName} = {left_gap_var.X}"
+        )
+
+        total_neighbors = 0
+        for var in real_node_neighbor_vars:
+            total_neighbors += var.X
+        print(f"{len(real_node_neighbor_vars)} neighbor vars. Sum/{total_neighbors=}")
+
+    # virtual_nodes = [n for n in ml_graph.layers_to_nodes[1] if n.is_virtual]
+    real_nodes = [n for n in ml_graph.layers_to_nodes[1] if not n.is_virtual]
+
+    rnodes_left_of_rnode_vars: dict[MLGNode, float] = {
+        rnode_right: sum(
+            ordering_gp_vars[rnode_left, rnode_right].X
+            for rnode_left in real_nodes
+            if rnode_left is not rnode_right
+        )
+        for rnode_right in real_nodes
+    }
+    print(f"{sorted(rnodes_left_of_rnode_vars.items(), key=lambda x: x[1])}")
 
 
 def _gen_order_var_and_constraints(m: gp.Model, nodes: list[MLGNode], prefix: str = ""):
