b8b072b91b0dc263cabd188c6944211e347d15d1
Jakob Roithinger <jake.roithinger@gmail.com>
2023-09-20 20:46:25 +0200

    changes to create result graphs

--- thesis_experiments/create_result_graphs.py
+++ thesis_experiments/create_result_graphs.py
@@ -4,19 +4,18 @@ import seaborn as sns
 
 # TODO aggregate csvs
 
-df = pd.read_csv(r"thesis_experiments\local_tests\testcase1\out.csv")
-
-
-sns.lineplot(data=df, x="real_nodes_per_layer_count ", y="time_s", hue="alg_name")
+df = pd.read_csv(r"thesis_experiments\local_tests\testcase2\out.csv")
+sns.lineplot(data=df, x="real_nodes_per_layer_count", y="crossings", hue="alg_name")
+# sns.lineplot(data=df, x="real_nodes_per_layer_count ", y="time_s", hue="alg_name")
 
 # Optionally, you can customize the plot using Seaborn functions or Matplotlib settings
 # For example, setting labels and title:
-plt.xlabel("X-Axis Label")
-plt.ylabel("Y-Axis Label")
-plt.title("Line Plot")
+plt.xlabel("Real node per Layer")
+plt.ylabel("Time taken to minimize")
+plt.title("OSCM")
 
 
-plt.legend(title="Z Values", loc="best")
+plt.legend(title="Algorithms", loc="best")
 
 # Show the plot
 plt.show()
