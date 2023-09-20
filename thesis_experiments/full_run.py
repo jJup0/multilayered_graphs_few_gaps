56005c772e26b9700a44e8b60716b056c84ed6f6
Jakob Roithinger <jake.roithinger@gmail.com>
2023-09-20 20:49:32 +0200

    refactored full_run.py

--- thesis_experiments/full_run.py
+++ thesis_experiments/full_run.py
@@ -7,10 +7,14 @@ cwd = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 thesis_experiments_dirname = "thesis_experiments"
 
 
-def run_regular(test_case_name: str):
-    in_dir_name = f"{thesis_experiments_dirname}/local_tests/{test_case_name}/in"
+def in_dir_name(test_case_name: str):
+    return f"{thesis_experiments_dirname}/local_tests/{test_case_name}/in"
+
+
+def create_graphs(test_case_name: str):
     graph_gen_count = 20
-    for real_node_count in (10, 20, 30, 40, 50):
+    # for real_node_count in (10, 20, 30, 40, 50):
+    for real_node_count in (10, 15, 20):
         virtual_node_count = real_node_count // 2
         generate_cmd_args = [
             "python",
@@ -20,11 +24,13 @@ def run_regular(test_case_name: str):
             f"{real_node_count}",
             f"{virtual_node_count}",
             "0.2",
-            in_dir_name,
+            in_dir_name(test_case_name),
         ]
         print(f"generating {graph_gen_count} graphs with {real_node_count=}")
         subprocess.run(generate_cmd_args, cwd=cwd, shell=True)
 
+
+def create_csv_out(test_case_name: str) -> str:
     out_csv_file = f"{thesis_experiments_dirname}/local_tests/{test_case_name}/out.csv"
     os.makedirs(os.path.dirname(out_csv_file), exist_ok=True)
 
@@ -41,6 +47,12 @@ def run_regular(test_case_name: str):
                 "time_s",
             )
         )
+    return out_csv_file
+
+
+def run_regular(test_case_name: str):
+    create_graphs(test_case_name)
+    out_csv_file = create_csv_out(test_case_name)
 
     minimize_cmd_args = [
         "python",
@@ -48,18 +60,19 @@ def run_regular(test_case_name: str):
         f"{thesis_experiments_dirname}.minimize_crossings",
         "--sidegaps",
         "--in_dir",
-        in_dir_name,
-        "median",
+        in_dir_name(test_case_name),
+        "<<alg_name>>",
         out_csv_file,
     ]
-    subprocess.run(minimize_cmd_args, cwd=cwd, shell=True)
+    for alg_name in ["median", "barycenter", "ilp"]:
+        minimize_cmd_args[-2] = alg_name
+        subprocess.run(minimize_cmd_args, cwd=cwd, shell=True)
 
-    minimize_cmd_args[-2] = "barycenter"
-    subprocess.run(minimize_cmd_args, cwd=cwd, shell=True)
 
-
-def create_batch_jobs(in_directory_path: str, out_csv_path: str):
-    files = os.listdir(in_directory_path)
+def run_batch(test_case_name: str):
+    create_graphs(test_case_name)
+    out_csv_file = create_csv_out(test_case_name)
+    files = os.listdir(in_dir_name(test_case_name))
 
     standard_run_cmds = [
         "qsub",
@@ -85,14 +98,14 @@ def create_batch_jobs(in_directory_path: str, out_csv_path: str):
         f"{thesis_experiments_dirname}.minimize_crossings",
         "--sidegaps",
         "--in_file",
-        f"{in_directory_path}",
+        f"<<file_name>>",
         "<<algorithm_name>>",
-        f"out_csv_path",
+        f"{out_csv_file}",
     ]
     for alg_name in ["median", "barycenter", "ilp"]:
         standard_run_cmds[-2] = alg_name
         for filename in files:
-            standard_run_cmds[-4] = filename
+            standard_run_cmds[-3] = filename
             subprocess.run(standard_run_cmds)
 
 
@@ -101,4 +114,4 @@ if __name__ == "__main__":
     #     os.path.realpath("./performance_tests/in"),
     #     os.path.realpath("./performance_tests/out/out2.csv"),
     # )
-    run_regular("testcase2")
+    run_regular("testcase_temp")
