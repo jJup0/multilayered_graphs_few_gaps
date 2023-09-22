361a14d11d4e24228bcddc98a49eecb8b6291723
Jakob <jake@acgroup.at>
2023-09-22 06:29:12 +0200

    added everything needed for minimize_crossings_wrapper.sh

--- thesis_experiments/minimize_crossings_wrapper.sh
+++ thesis_experiments/minimize_crossings_wrapper.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
-python -m thesis_experiments.minimize_crossings "@"
+echo "I am running"
+sync
+
+if [ -z "$PYTHONPATH" ]; then
+    export PYTHONPATH=$HOME/multilayered_graphs_few_gaps
+fi
+
+
+echo "python path: " $PYTHONPATH
+sync
+
+
+# GUROBI
+export GUROBI_HOME="/home1/share/gurobi/gurobi/linux64"
+export PATH="${PATH}:${GUROBI_HOME}/bin"
+export GRB_LICENSE_FILE="/home1/share/gurobi/gurobi.lic"
+if [ -n "$LD_LIBRARY_PATH" ]; then
+  export LD_LIBRARY_PATH="${GUROBI_HOME}/lib"
+else
+  export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
+fi
+
+/home1/e52009269/.pyenv/versions/3.10.9/bin/python -m thesis_experiments.minimize_crossings "$@"
