fbf7cbffa25115a3042510c4f4d3734fb616db2b
Jakob Roithinger <jake.roithinger@gmail.com>
2023-09-24 17:13:19 +0200

    changed input params for generate 2 layer test instances

--- thesis_experiments/generate_2_layer_test_instances.py
+++ thesis_experiments/generate_2_layer_test_instances.py
@@ -10,45 +10,45 @@ import sys
 
 from multilayered_graph.multilayered_graph import MLGNode, MultiLayeredGraph
 
+
 def generate_oscm_graph(
     nodes_per_layer: int,
-    virtual_node_count: int,
-    regular_edge_density: float,
-    # *,
-    # two_sided: bool = False
+    virtual_node_ratio: float,
+    average_node_degree: float,
 ) -> MultiLayeredGraph:
-    assert nodes_per_layer >= 0, f"Invalid {nodes_per_layer=}"
-    assert virtual_node_count >= 0, f"Invalid {virtual_node_count=}"
-    assert 0 <= regular_edge_density <= 1, f"Invalid {regular_edge_density=}"
+    assert 0 <= nodes_per_layer, f"Invalid {nodes_per_layer=}"
+    assert 0 <= virtual_node_ratio <= 1, f"Invalid {virtual_node_ratio=}"
+    assert 0 <= average_node_degree, f"Invalid {average_node_degree=}"
 
-    regular_edges_count = int(nodes_per_layer * nodes_per_layer * regular_edge_density)
+    virtual_nodes_per_layer = int(nodes_per_layer * virtual_node_ratio)
+    real_nodes_per_layer = nodes_per_layer - virtual_nodes_per_layer
 
+    # real nodes
     ml_graph = MultiLayeredGraph(2)
-    for _ in range(nodes_per_layer):
-        ml_graph.add_real_node(0)
-    for _ in range(nodes_per_layer):
-        ml_graph.add_real_node(1)
+    for layer_idx in [0, 1]:
+        for _ in range(real_nodes_per_layer):
+            ml_graph.add_real_node(layer_idx)
 
+    # edges between real nodes
     all_edges: list[tuple[MLGNode, MLGNode]] = [
         (n1, n2)
         for n1 in ml_graph.layers_to_nodes[0]
         for n2 in ml_graph.layers_to_nodes[1]
     ]
     random.shuffle(all_edges)
+    regular_edges_count = int(real_nodes_per_layer * average_node_degree)
     for i in range(regular_edges_count):
         n1, n2 = all_edges[i]
-        ml_graph.add_edge(n1, n2)  # type: ignore # protected
+        ml_graph.add_edge(n1, n2)
 
-    # create virtual nodes first
+    # create virtual nodes
     vnodes_l1: set[MLGNode] = set()
     vnodes_l2: set[MLGNode] = set()
-    for i in range(virtual_node_count):
+    for i in range(virtual_nodes_per_layer):
         vnodes_l1.add(ml_graph.add_virtual_node(0, f"{i}"))
         vnodes_l2.add(ml_graph.add_virtual_node(1, f"{i}"))
 
     # then add edges for each virtual node
-    all_nodes_l2 = list(ml_graph.layers_to_nodes[1])
-
     def check_vnodes_have_single_neighbor(curr_node: MLGNode | None = None):
         for vnode in [n for n in ml_graph.all_nodes_as_list() if n.is_virtual]:
             assert (
@@ -56,6 +56,7 @@ def generate_oscm_graph(
                 + len(ml_graph.nodes_to_out_edges[vnode])
             ) <= 1, f"{vnode=} got second neighbor. || {curr_node=}"
 
+    all_nodes_l2 = list(ml_graph.layers_to_nodes[1])
     for vnode1 in vnodes_l1:
         neighbor_in_l2 = random.choice(all_nodes_l2)
         ml_graph.add_edge(vnode1, neighbor_in_l2)
@@ -63,7 +64,6 @@ def generate_oscm_graph(
             all_nodes_l2.remove(neighbor_in_l2)  # O(n) operation
             vnodes_l2.remove(neighbor_in_l2)
         check_vnodes_have_single_neighbor(vnode1)
-        # temp check
 
     real_nodes_l1 = [n for n in ml_graph.layers_to_nodes[0] if not n.is_virtual]
     for vnode2 in vnodes_l2:
