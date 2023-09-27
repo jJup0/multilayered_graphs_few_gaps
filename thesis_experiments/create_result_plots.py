d35f54a5109cf777b6a1fb5f0d29e051194e83b6
Jakob <jake@acgroup.at>
2023-09-27 08:04:27 +0200

    several changes to create result plots

--- thesis_experiments/create_result_plots.py
+++ thesis_experiments/create_result_plots.py
@@ -11,6 +11,8 @@ import logging
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 
+# TODO automatically make crossings and time plot
+# TODO make ratio to ILP crossings plot
 
 
 class TestCaseInfo(TypedDict):
@@ -22,31 +24,39 @@ def create_graph(test_case_directory:str):
     with open(os.path.join(test_case_directory, "info.json")) as f:
         test_case_info: TestCaseInfo = json.load(f)
 
-    y_data_str = "crossings"
-    # y_data_str = "time_s"
+    df = pd.read_csv(os.path.join(test_case_directory, "out.csv"))
     x_data_str = test_case_info["variable"][0].replace(" ", "_")
 
-    df = pd.read_csv(os.path.join(test_case_directory, "out.csv"))
-    sns.lineplot(data=df, x=x_data_str, y=y_data_str, hue="alg_name")
+    for y_data_str in [ "crossings", "time_s"]:
+        sns.lineplot(data=df, x=x_data_str, y=y_data_str, hue="alg_name")
 
-    plt.xlabel(x_data_str)
-    plt.ylabel(y_data_str)
-    plt.title(test_case_info["graph_title"])
+        plt.xlabel(x_data_str)
+        plt.ylabel(y_data_str)
+        plt.title(test_case_info["graph_title"])
+        # plt.yscale("log")
+        plt.legend(title="Algorithms", loc="best")
 
-    # plt.yscale("log")
+        # save plot to disk
+        _, test_case_name = os.path.split(os.path.realpath(test_case_directory))
+        plt.savefig(os.path.join(f"{test_case_name}_{y_data_str}.png"))
+        plt.clf()
+    
+    # TODO ratio
+    # Separate the data into optimal and heuristic datasets
+    optimal_data = df[df["alg_name"] == "optimal"]
+    heuristic_data = df[df["alg_name"] != "optimal"]
 
-    plt.legend(title="Algorithms", loc="best")
+    # Calculate the ratio of heuristic results to the optimum
+    heuristic_data["ratio"] = heuristic_data[y_data_str] / optimal_data[y_data_str].values[0]
+    sns.lineplot(data=heuristic_data, x=x_data_str, y="ratio", hue="alg_name")
 
 
-    _, test_case_name = os.path.split(os.path.realpath(test_case_directory))
-    # Show the plot
-    plt.savefig(os.path.join(f"{test_case_name}.png"))
-
-
-if __name__ == "__main__":
-    assert len(sys.argv) > 1
-    test_case_name_match = sys.argv[1]
-
+def find_matching_test_case_dirs_and_plot_data(test_case_name_match:str):
+    """
+    Finds all testcase directories with `test_case_name_match`
+    as a substring and calls create_graph().
+    """
+    
     logger.info("creating plots")
     test_case_root_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "local_tests")
     found_test_cases = 0
@@ -60,3 +70,10 @@ if __name__ == "__main__":
             create_graph(test_case_dir_path)
     if found_test_cases == 0:
         logger.warning("no testcases found matching %s in %s", test_case_name_match, os.listdir(test_case_root_dir))
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) > 1
+    test_case_name_match = sys.argv[1]
+    # argv[1] should be a substring to search testcases for
+    find_matching_test_case_dirs_and_plot_data(test_case_name_match)
