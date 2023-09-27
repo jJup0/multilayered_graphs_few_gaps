a468237666dc8ce7efbed95085267d3df7f4d227
Jakob <jake@acgroup.at>
2023-09-27 17:50:43 +0200

    modifications to create_result_plots

--- thesis_experiments/create_result_plots.py
+++ thesis_experiments/create_result_plots.py
@@ -42,8 +42,15 @@ def out_dir(test_case_name_match: str, test_case_directory: str):
 def create_graph(test_case_name_match: str, test_case_directory: str):
     with open(os.path.join(test_case_directory, "info.json")) as f:
         test_case_info: TestCaseInfo = json.load(f)
-
-    df = pd.read_csv(os.path.join(test_case_directory, "out.csv"))
+    csv_real_file_path = os.path.join(test_case_directory, "out.csv")
+    try:
+        df = pd.read_csv(csv_real_file_path)
+    except Exception as err:
+        str_err = str(err)
+        if "Error tokenizing data. C error: Expected " in str_err:
+            line_str_idx = str_err.find("line")
+            line_nr = int(str_err[line_str_idx + 4 : str_err.find(",", line_str_idx)])
+            raise Exception(f"{csv_real_file_path}:{line_nr} has faulty row: {err}")
     x_data_str = test_case_info["variable"][0].replace(" ", "_")
 
     _, test_case_name = os.path.split(os.path.realpath(test_case_directory))
@@ -69,25 +76,30 @@ def create_graph(test_case_name_match: str, test_case_directory: str):
     # optimal_data = df[df["alg_name"] == "ilp"]
 
     # calculate ilp ratio
-    df["ratio"] = -1
+    df["ratio-crossings"] = -1
+    df["ratio-time_s"] = -1
     heuristic_df = df[df["alg_name"] != "ilp"]
     for index, row in heuristic_df.iterrows():
-        row["ratio"] = 1
         ilp_rows = df[
             (df["alg_name"] == "ilp")
             & (df["instance_name"] == row["instance_name"])
+            & (df["gap_type"] == row["gap_type"])
             & (df["gap_count"] == row["gap_count"])
         ]
-        assert len(ilp_rows) == 1, (
-            f"NOT EXACTLY ONE RESULT FOR ILP: {ilp_rows.head()=}"
-            f'{row["instance_name"], }'
-        )
-        heuristic_df.at[index, "ratio"] = (
+        assert (
+            len(ilp_rows) == 1
+        ), f"NOT EXACTLY ONE RESULT FOR ILP: {len(ilp_rows)=}\n{row['instance_name']=}\n{csv_real_file_path}:{index}"
+        heuristic_df.at[index, "ratio-crossings"] = (
             row["crossings"] / ilp_rows.iloc[0]["crossings"]
         )
+        heuristic_df.at[index, "ratio-time_s"] = (
+            row["time_s"] / ilp_rows.iloc[0]["time_s"]
+        )
 
-    for y_data_str in ["crossings"]:
-        sns.lineplot(data=heuristic_df, x=x_data_str, y="ratio", hue="alg_name")
+    for y_data_str in ["crossings", "time_s"]:
+        sns.lineplot(
+            data=heuristic_df, x=x_data_str, y=f"ratio-{y_data_str}", hue="alg_name"
+        )
 
         plt.xlabel(x_data_str)
         plt.ylabel(f"{y_data_str} ratio to ILP")
@@ -130,8 +142,7 @@ def find_matching_test_case_dirs_and_plot_data(test_case_name_match: str):
 
 
 if __name__ == "__main__":
-    # assert len(sys.argv) > 1
-    # test_case_name_match = sys.argv[1]
-    test_case_name_match = "v7"
+    assert len(sys.argv) > 1
+    test_case_name_match = sys.argv[1]
     # argv[1] should be a substring to search testcases for
     find_matching_test_case_dirs_and_plot_data(test_case_name_match)
