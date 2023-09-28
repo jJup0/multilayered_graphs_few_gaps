8b1d03f00c95b83b4effb1915353d50cde84962c
Jakob <jake@acgroup.at>
2023-09-28 08:09:27 +0200

    added some filter functions to create_result_plots

--- thesis_experiments/create_result_plots.py
+++ thesis_experiments/create_result_plots.py
@@ -8,12 +8,14 @@ from typing import TypedDict
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import io
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 
 # filter out FutureWarning
 warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", module="plt.legend")
 
 
 test_case_root_dir = os.path.join(
@@ -25,6 +27,7 @@ class TestCaseInfo(TypedDict):
     constants: dict[str, int | float]
     variable: tuple[str, list[int | float]]
     graph_title: str
+    expected_results_count: int
 
 
 def out_dir(test_case_name_match: str, test_case_directory: str):
@@ -39,10 +42,45 @@ def out_dir(test_case_name_match: str, test_case_directory: str):
     )
 
 
-def create_graph(test_case_name_match: str, test_case_directory: str):
-    with open(os.path.join(test_case_directory, "info.json")) as f:
-        test_case_info: TestCaseInfo = json.load(f)
-    csv_real_file_path = os.path.join(test_case_directory, "out.csv")
+def _read_csv(csv_real_file_path: str) -> pd.DataFrame:
+    filtered_lines: list[str] = []
+
+    with open(csv_real_file_path, "r") as f:
+        header = f.readline()
+        filtered_lines.append(header)
+        comma_count = header.count(",")
+        for line in f:
+            if line.count(",") == comma_count:
+                filtered_lines.append(line)
+
+            # try:
+            #     # Attempt to parse the line as CSV
+            #     row = pd.read_csv(io.StringIO(line))
+            #     data.append(row)
+            # except pd.errors.ParserError:
+            #     # Handle the parsing error (e.g., skip the line)
+            #     print(f"Skipped line due to parsing error: {line.strip()}")
+            # except pd.errors.EmptyDataError:
+            #     print(f"Skipped line due to parsing error: {line.strip()}")
+    # # Concatenate the valid rows into a DataFrame
+    # df = pd.concat(data, ignore_index=True)
+
+    filtered_out_path = os.path.join(
+        os.path.dirname(csv_real_file_path), "filtered_out.csv"
+    )
+    with open(filtered_out_path, "w") as f:
+        f.writelines(filtered_lines)
+
+    try:
+        df = pd.read_csv(filtered_out_path)
+    except pd.errors.ParserError as err:
+        logger.error("failed to read %s", filtered_out_path)
+        raise err
+
+    return df
+
+
+def _read_csv_no_filter(csv_real_file_path: str) -> pd.DataFrame:
     try:
         df = pd.read_csv(csv_real_file_path)
     except Exception as err:
@@ -51,7 +89,58 @@ def create_graph(test_case_name_match: str, test_case_directory: str):
             line_str_idx = str_err.find("line")
             line_nr = int(str_err[line_str_idx + 4 : str_err.find(",", line_str_idx)])
             raise Exception(f"{csv_real_file_path}:{line_nr} has faulty row: {err}")
+        raise err
+    return df
+
+
+def remove_incomplete_data_sets(df: pd.DataFrame):
+    ...
+
+
+def create_graph(test_case_name_match: str, test_case_directory: str):
+    # read info file
+    with open(os.path.join(test_case_directory, "info.json")) as f:
+        test_case_info: TestCaseInfo = json.load(f)
     x_data_str = test_case_info["variable"][0].replace(" ", "_")
+    expected_row_count = test_case_info["expected_results_count"]
+
+    # read csv
+    csv_real_file_path = os.path.join(test_case_directory, "out.csv")
+    df = _read_csv(csv_real_file_path)
+
+    # filter invalid data
+    original_row_count = len(df)
+
+    # # filter faulty lines: faulty := empty or only an integer in one column
+    # conditions = (df.notnull().all(axis=1)) & (df.apply(lambda x: len(x.dropna())) > 1)
+
+    # # apply the conditions to filter the DataFrame
+    # df = df[conditions]
+
+    # # count the number of rows after filtering
+    # filtered_row_count = len(df)
+
+    # # Calculate the number of rows that were filtered out
+    # filtered_out_count = original_row_count - filtered_row_count
+
+    # # Display a warning message
+    # if filtered_out_count > 0:
+    #     logger.warning(
+    #         f"{filtered_out_count} rows were filtered out due to missing or single-column data."
+    #     )
+    if original_row_count != expected_row_count:
+        _, test_case_name = os.path.split(test_case_directory)
+        logger.warning(
+            f"Expected {expected_row_count} rows, received {original_row_count} for {test_case_name}"
+        )
+        remove_incomplete_data_sets(df)
+        return
+
+    # write filtered back to disk
+    # df.to_csv(
+    #     os.path.join(os.path.dirname(csv_real_file_path), "filtered_out.csv"),
+    #     index=False,
+    # )
 
     _, test_case_name = os.path.split(os.path.realpath(test_case_directory))
     for y_data_str in ["crossings", "time_s"]:
