4e58bd7e98e896047d5108c3109b46824f0ed398
Jakob <jake@acgroup.at>
2023-10-17 18:01:52 +0200

    wrote case_study_wrapper.sh

---
+++ thesis_experiments/case_study_wrapper.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -z "$PYTHONPATH" ]; then
+    export PYTHONPATH=$HOME/multilayered_graphs_few_gaps
+fi
+
+# GUROBI
+export GUROBI_HOME="/home1/share/gurobi/gurobi/linux64"
+export PATH="${PATH}:${GUROBI_HOME}/bin"
+export GRB_LICENSE_FILE="/home1/share/gurobi/gurobi.lic"
+if [ -n "$LD_LIBRARY_PATH" ]; then
+  export LD_LIBRARY_PATH="${GUROBI_HOME}/lib"
+else
+  export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
+fi
+
+/home1/e52009269/.pyenv/versions/3.10.9/bin/python -m thesis_experiments.case_study $@
